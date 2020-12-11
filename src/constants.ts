4602a6d996e06f62ff9ccbe44d778fada07001d8
James Lynch <lynchjames@gmail.com>
2020-12-11 00:35:26 +0000

    Updates front matter regex101 reference url

--- src/constants.ts
+++ src/constants.ts
@@ -4,5 +4,5 @@ export const MD_VIEW_TYPE = 'markdown';
 // https://regex101.com/r/gw85cc/2
 export const INTERNAL_LINK_REGEX = /\[\[(?<wikitext>.*)\]\]|<a href="(?<mdpath>.*)">(?<mdtext>.*)<\/a>/gim;
 
-// https://regex101.com/r/Yg7HuO/1
+// https://regex101.com/r/Yg7HuO/2
 export const FRONT_MATTER_REGEX = /^(---)$.+?^(---)$.+?/ims;
